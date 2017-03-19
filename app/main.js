edb90bbb0d6758e4bb73a08000c5a94a8ed94b4e
Noim <nilsbergmann@noim.io>
2017-03-19 13:01:42 +0100

    Init

--- app/main.js
+++ app/main.js
@@ -1,1 +1,352 @@
 // Your code
+import './main.css';
+
+import angular from 'angular';
+import ngMaterial from 'angular-material';
+import uiRouter from 'angular-ui-router';
+
+import Chartist from 'chartist';
+
+import 'chartist/dist/chartist.min.css';
+
+import "angular-material/angular-material.min.css";
+
+import "chartist-plugin-fill-donut";
+
+import io from 'socket.io-client';
+
+const app = angular.module('SpeedTest', [ngMaterial, uiRouter]);
+
+app.controller('Main', function ($scope, Socket) {
+
+});
+
+app.service('Socket', function ($rootScope, $mdToast) {
+    const _this = this;
+    this.socket = io('//:3001');
+    this.socket.on('PushData', payload => {
+        if (payload.doc && payload.doc.finished) {
+            console.log(`New speedtest result.`);
+            $mdToast.showSimple("Added new speedtest result!");
+            console.log(payload.doc);
+            this.FinishedSpeedTests.push(payload.doc);
+            this.FinishedSpeedTests = this.FinishedSpeedTests.sortByDateGetBiggerGerman();
+            this.makeChartData();
+            this.updateChart();
+
+        } else if (payload.doc && !payload.doc.finished) {
+            console.log(`New speedtest started... Waiting for results...`);
+            $mdToast.showSimple("New speedtest started!");
+            console.log(payload.doc);
+        } else {
+            console.log(`I don't know what this is.`);
+            console.log(payload);
+        }
+    });
+    this.Data = false;
+    this.FinishedSpeedTests = [];
+    this.socket.on('AllData', payload => {
+        this.Data = true;
+        for (let i = 0; i < payload.rows.length; i++) {
+            if (payload.rows[i].doc && payload.rows[i].doc.finished) {
+                _this.FinishedSpeedTests.push(payload.rows[i].doc);
+            }
+        }
+        _this.FinishedSpeedTests = _this.FinishedSpeedTests.sortByDateGetBiggerGerman();
+        console.log(payload);
+        console.log(_this.FinishedSpeedTests);
+        this.makeChartData();
+        $rootScope.$emit('AllData');
+    });
+    this.ChartData = [];
+    this.updateChart = function () {
+        $rootScope.$emit('updateChart');
+    };
+    this.createChart = function () {
+        $rootScope.$emit('createChart');
+    };
+    this.makeChartData = function () {
+        const labels = [];
+        const Upload = [];
+        const Download = [];
+        const Ping = [];
+        for (let i = 0; i < this.FinishedSpeedTests.length; i++) {
+            Upload.push({
+                meta: this.FinishedSpeedTests[i].upload,
+                value: (this.FinishedSpeedTests[i].uploadBytes / (1024*1024)).toFixed(2),
+                org: this.FinishedSpeedTests[i].uploadBytes
+            });
+            Download.push({
+                meta: this.FinishedSpeedTests[i].download,
+                value: (this.FinishedSpeedTests[i].downloadBytes / (1024*1024)).toFixed(2),
+                org: this.FinishedSpeedTests[i].downloadBytes
+            });
+            Ping.push({
+                meta: this.FinishedSpeedTests[i].ping,
+                value: this.FinishedSpeedTests[i].ping
+            });
+            const d = new Date(this.FinishedSpeedTests[i].createdAt);
+            //labels.push(`${d.getHours() < 10 ? "0" + d.getHours() : d.getHours()}:${d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()}`);
+            labels.push(d.getTime());
+        }
+        this.labels = labels;
+        this.Download = Download;
+        this.Upload = Upload;
+        this.Ping = Ping;
+    };
+});
+
+app.controller('loading', function ($scope, Socket, $state, $rootScope) {
+    if (Socket.Data) {
+        $state.go('home');
+    } else {
+        benchmark((next) => {
+            $rootScope.$on('AllData', (e) => {
+                next(function (start, end, difference) {
+                    if (difference.ms < 1400) {
+                        setTimeout(function () {
+                            $state.go('home');
+                        }, 1400 - difference.ms);
+                        console.log("Wait " + (1400 - difference.ms) + "ms");
+                    } else {
+                        $state.go('home');
+                    }
+                });
+            });
+        });
+    }
+});
+
+app.controller('home', function ($scope, Socket, $rootScope, $state) {
+    if (!Socket.Data) {
+        $state.go('loading');
+        return;
+    }
+    $scope.safeApply = function(fn) {
+        var phase = this.$root.$$phase;
+        if(phase == '$apply' || phase == '$digest') {
+            if(fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+    const DLch = new Chartist.Line('#DownloadChart', {
+        labels: Socket.labels,
+        series: [Socket.Download]
+    }, {
+        axisX: {
+            showLabel: false,
+            showGrid: false
+        },
+        plugins: [
+        ]
+    });
+    const UPch = new Chartist.Line('#UploadChart', {
+        labels: Socket.labels,
+        series: [Socket.Upload]
+    }, {
+        axisX: {
+            showLabel: false,
+            showGrid: false
+        },
+        plugins: [
+        ]
+    });
+    const Pingch = new Chartist.Line('#PingChart', {
+        labels: Socket.labels,
+        series: [Socket.Ping]
+    }, {
+        axisX: {
+            showLabel: false,
+            showGrid: false
+        },
+        plugins: [
+        ]
+    });
+    const LastDownloadChart = new Chartist.Pie('#LastDownloadChart', {
+        series: [Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].value || 0 ],
+        labels: ['', '']
+    }, {
+        donut: true,
+        donutWidth: 20,
+        startAngle: 210,
+        total: Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].value < 5 ? 5 : 125,
+        showLabel: false,
+        plugins: [
+            Chartist.plugins.fillDonut({
+                items: [{
+                    content: '<i class="fa fa-tachometer"></i>',
+                    position: 'bottom',
+                    offsetY : 10,
+                    offsetX: -2
+                }, {
+                    content: '<h3>'+ Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].meta || 0 +'</h3>'
+                }]
+            })
+        ],
+    });
+    const LastUploadChart = new Chartist.Pie('#LastUploadChart', {
+        series: [Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].value || 0 ],
+        labels: ['', '']
+    }, {
+        donut: true,
+        donutWidth: 20,
+        startAngle: 210,
+        total: Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].value < 5 ? 5 : 125,
+        showLabel: false,
+        plugins: [
+            Chartist.plugins.fillDonut({
+                items: [{
+                    content: '<i class="fa fa-tachometer"></i>',
+                    position: 'bottom',
+                    offsetY : 10,
+                    offsetX: -2
+                }, {
+                    content: '<h3>'+ Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].meta || 0 +'</h3>'
+                }]
+            })
+        ],
+    });
+    $scope.LastPing = Socket.Ping[Socket.Ping.length > 0 ? Socket.Ping.length - 1 : 0].value || 0;
+    $scope.LastTest = new Date(Socket.labels[Socket.labels.length > 0 ? Socket.labels.length - 1 : 0] || 0);
+    $scope.safeApply();
+    $rootScope.$on('updateChart', () => {
+        DLch.update({
+            labels: Socket.labels,
+            series: [Socket.Download]
+        });
+        UPch.update({
+            labels: Socket.labels,
+            series: [Socket.Upload]
+        });
+        Pingch.update({
+            labels: Socket.labels,
+            series: [Socket.Ping]
+        });
+        LastDownloadChart.update({
+            series: [Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].value || 0 ],
+            labels: ['', '']
+        }, {
+            donut: true,
+            donutWidth: 20,
+            startAngle: 210,
+            total: Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].value < 5 ? 5 : 125,
+            showLabel: false,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<i class="fa fa-tachometer"></i>',
+                        position: 'bottom',
+                        offsetY : 10,
+                        offsetX: -2
+                    }, {
+                        content: '<h3>'+ Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].meta || 0 +'</h3>'
+                    }]
+                })
+            ],
+        });
+        LastUploadChart.update({
+            series: [Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].value || 0 ],
+            labels: ['', '']
+        }, {
+            donut: true,
+            donutWidth: 20,
+            startAngle: 210,
+            total: Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].value < 5 ? 5 : 125,
+            showLabel: false,
+            plugins: [
+                Chartist.plugins.fillDonut({
+                    items: [{
+                        content: '<i class="fa fa-tachometer"></i>',
+                        position: 'bottom',
+                        offsetY : 10,
+                        offsetX: -2
+                    }, {
+                        content: '<h3>'+ Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].meta || 0 +'</h3>'
+                    }]
+                })
+            ],
+        });
+        $scope.LastPing = Socket.Ping[Socket.Ping.length > 0 ? Socket.Ping.length - 1 : 0].value || 0;
+        $scope.LastTest = new Date(Socket.labels[Socket.labels.length > 0 ? Socket.labels.length - 1 : 0] || 0);
+        $scope.safeApply();
+        console.log("Chart updated.");
+    });
+});
+
+app.config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+
+    $urlRouterProvider.otherwise('/loading');
+    $stateProvider.state({
+        name: "loading",
+        url: "/loading",
+        controller: 'loading',
+        template: require('./loading.html')
+    });
+    $stateProvider.state({
+        name: "home",
+        url: "/",
+        controller: "home",
+        template: require('./home.html')
+    });
+
+    $mdThemingProvider.theme('default').dark();
+});
+
+function benchmark(method) {
+    const start = +(new Date);
+    method && method(function (callback) {
+        const end = +(new Date);
+        const difference = end - start;
+        callback && callback(start, end, {
+            milliseconds: difference,
+            ms: difference,
+            seconds: (difference / 1000) % 60,
+            minutes: (difference / (1000 * 60)) % 60,
+            hours: (difference / (1000 * 60 * 60)) % 24
+        });
+    });
+}
+
+function ctPointLabels(options) {
+    return function ctPointLabels(chart) {
+        var defaultOptions = {
+            labelClass: 'ct-label',
+            labelOffset: {
+                x: 0,
+                y: -10
+            },
+            textAnchor: 'middle'
+        };
+
+        options = Chartist.extend({}, defaultOptions, options);
+
+        if(chart instanceof Chartist.Line) {
+            chart.on('draw', function(data) {
+                if(data.type === 'point') {
+                    data.group.elem('text', {
+                        x: data.x + options.labelOffset.x,
+                        y: data.y + options.labelOffset.y,
+                        style: 'text-anchor: ' + options.textAnchor
+                    }, options.labelClass).text(data.meta);
+                }
+            });
+        }
+    }
+}
+
+Array.prototype.sortByDateGetBiggerGerman = function () {
+    return this.sort(function (a, b) {
+        if (parseDate(a.createdAt).getTime() > parseDate(b.createdAt).getTime()) return 1;
+        if (parseDate(a.createdAt).getTime() < parseDate(b.createdAt).getTime()) return -1;
+        return 0;
+    });
+};
+
+function parseDate(input) {
+    var parts = input.match(/(\d+)/g);
+    // note parts[1]-1
+    return new Date(parts[2], parts[1]-1, parts[0]);
+}
--- server.js
+++ server.js
@@ -1,5 +1,25 @@
 /* eslint no-console: 0 */
 
+const jsome = require('jsome');
+jsome.level.show = true;
+
+const PouchDB = require('pouchdb');
+
+PouchDB.plugin(require('pouchdb-find'));
+
+const db = new PouchDB('speedtest');
+const DBError = require('debug')('DB:error');
+
+db.createIndex({
+    index: {
+        fields: ["createdAt"]
+    }
+});
+
+db.info().then(function (info) {
+    jsome(info);
+});
+
 const path = require('path');
 const express = require('express');
 const webpack = require('webpack');
@@ -8,40 +28,117 @@ const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./webpack.config.js');
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
-const port = isDeveloping ? 3000 : process.env.PORT;
+const port = isDeveloping ? 3000 : process.env.PORT ? process.env.PORT : 3000;
 const app = express();
+const io = require('socket.io')();
+const ioDebug = require('debug')('IO');
+const ioError = require('debug')('IO:error');
+
+const speedTest = require('speedtest-net');
+
+const uuid = require('node-uuid');
+
+const TaskError = require('debug')('Task:error');
+const TaskDebug = require('debug')('Task');
+
+const SpeedTestCLIWrapper = require('./lib/SpeedtestCLIWrapper')();
 
 if (isDeveloping) {
-  const compiler = webpack(config);
-  const middleware = webpackMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-    contentBase: 'src',
-    stats: {
-      colors: true,
-      hash: false,
-      timings: true,
-      chunks: false,
-      chunkModules: false,
-      modules: false
-    }
-  });
+    const compiler = webpack(config);
+    const middleware = webpackMiddleware(compiler, {
+        publicPath: config.output.publicPath,
+        contentBase: 'src',
+        stats: {
+            colors: true,
+            hash: false,
+            timings: true,
+            chunks: false,
+            chunkModules: false,
+            modules: false
+        }
+    });
 
-  app.use(middleware);
-  app.use(webpackHotMiddleware(compiler));
-  app.get('*', function response(req, res) {
-    res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
-    res.end();
-  });
+    app.use(middleware);
+    app.use(webpackHotMiddleware(compiler));
+    app.get('*', function response(req, res) {
+        res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
+        res.end();
+    });
 } else {
-  app.use(express.static(__dirname + '/dist'));
-  app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
+    app.use(express.static(__dirname + '/dist'));
+    app.get('*', function response(req, res) {
+        res.sendFile(path.join(__dirname, 'dist/index.html'));
+    });
 }
 
+function test() {
+    const id = uuid.v1();
+    TaskDebug(`Start test with uuid ${id}`);
+    db.put({
+        _id: id,
+        createdAt: new Date(),
+        finished: false
+    }).then(() => {
+        return SpeedTestCLIWrapper.test().then(result => {
+            TaskDebug(`Test (${id}) finished. Result: ${result.ping} | ${result.download} | ${result.upload} => Add to database`);
+            return db.get(id).then(doc => {
+                doc.download = result.download;
+                doc.upload = result.upload;
+                doc.ping = result.ping;
+                doc.downloadBytes = result.downloadBytes;
+                doc.uploadBytes = result.uploadBytes;
+                doc.raw = result.raw;
+                doc.finished = true;
+                return db.put(doc);
+            });
+        }).catch(e => {
+            TaskError(`Something went wrong while testing. Save to database. Error: ${e}.`);
+            return db.get(id).then(doc => {
+                doc.download = -1;
+                doc.upload = -1;
+                doc.ping = -1;
+                doc.downloadBytes = -1;
+                doc.uploadBytes = -1;
+                doc.raw = null;
+                doc.error = e;
+                doc.finished = true;
+            }).catch(TaskError);
+        });
+    }).catch(TaskError);
+}
+setInterval(function () {
+    test();
+}, 1 * 60 * 1000);
+test();
+io.on('connection', function (client) {
+    ioDebug(`New client with id ${client.conn.id}`);
+    let changes = db.changes({
+        since: 'now',
+        live: true,
+        include_docs: true
+    });
+    changes.on('change', (change) => {
+        ioDebug(`Push new data to client with id ${client.conn.id}`);
+        client.emit('PushData', change);
+    }).on('error', (err) => {
+        DBError(err);
+    });
+    db.allDocs({include_docs: true}).then(result => {
+        client.emit('AllData', result);
+    }).catch(ioError);
+    client.on('disconnect', () => {
+        changes.cancel();
+        changes = null;
+        ioDebug(`Removed listeners for client ${client.conn.id}.`);
+        client = null;
+    });
+});
+
+io.listen(3001);
+
 app.listen(port, '0.0.0.0', function onStart(err) {
-  if (err) {
-    console.log(err);
-  }
-  console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+    if (err) {
+        console.log(err);
+    }
+    console.info('==> 🌎 Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
 });
