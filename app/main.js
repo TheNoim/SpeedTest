4dce55fb732c39bcbe31aa64cf8c4de515819aad
Noim <nilsbergmann@noim.io>
2017-03-21 12:44:56 +0100

    [WIP] Many improvements

--- app/main.js
+++ app/main.js
@@ -17,6 +17,8 @@ import io from 'socket.io-client';
 
 import moment from 'moment';
 
+import prettierBytes from 'prettier-bytes';
+
 const app = angular.module('SpeedTest', [ngMaterial, uiRouter]);
 
 app.controller('Main', function ($scope, Socket) {
@@ -71,6 +73,9 @@ app.service('Socket', function ($rootScope, $mdToast) {
         const labels = [];
         const Upload = [];
         const Download = [];
+        let DownloadAVG = 0;
+        let UploadAVG = 0;
+        let PingAVG = 0;
         const Ping = [];
         for (let i = 0; i < this.FinishedSpeedTests.length; i++) {
             Upload.push({
@@ -90,7 +95,15 @@ app.service('Socket', function ($rootScope, $mdToast) {
             const d = new Date(this.FinishedSpeedTests[i].createdAt);
             //labels.push(`${d.getHours() < 10 ? "0" + d.getHours() : d.getHours()}:${d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()}`);
             labels.push(d.getTime());
+            DownloadAVG += this.FinishedSpeedTests[i].downloadBytes;
+            UploadAVG += this.FinishedSpeedTests[i].uploadBytes;
+            PingAVG += this.FinishedSpeedTests[i].ping;
         }
+        this.AVG = {
+            Download: DownloadAVG / this.FinishedSpeedTests.length,
+            Upload: UploadAVG / this.FinishedSpeedTests.length,
+            Ping: PingAVG / this.FinishedSpeedTests.length
+        };
         this.labels = labels;
         this.Download = Download;
         this.Upload = Upload;
@@ -169,7 +182,7 @@ app.controller('home', function ($scope, Socket, $rootScope, $state) {
     });
     const LastDownloadChart = new Chartist.Pie('#LastDownloadChart', {
         series: [Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].value || 0 ],
-        labels: ['', '']
+        labels: ['']
     }, {
         donut: true,
         donutWidth: 20,
@@ -191,7 +204,7 @@ app.controller('home', function ($scope, Socket, $rootScope, $state) {
     });
     const LastUploadChart = new Chartist.Pie('#LastUploadChart', {
         series: [Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].value || 0 ],
-        labels: ['', '']
+        labels: ['']
     }, {
         donut: true,
         donutWidth: 20,
@@ -211,6 +224,51 @@ app.controller('home', function ($scope, Socket, $rootScope, $state) {
             })
         ],
     });
+    const AVGDownloadChart = new Chartist.Pie('#AVGDownloadChart', {
+        series: [Socket.AVG.Download || 0 ],
+        labels: ['']
+    }, {
+        donut: true,
+        donutWidth: 20,
+        startAngle: 210,
+        total: Socket.AVG.Download <= 5242880 ? 5242880 : 131072000, //5242880
+        showLabel: false,
+        plugins: [
+            Chartist.plugins.fillDonut({
+                items: [{
+                    content: '<i class="fa fa-tachometer"></i>',
+                    position: 'bottom',
+                    offsetY : 10,
+                    offsetX: -2
+                }, {
+                    content: '<h3 id="AVGDownloadChartH3">'+ prettierBytes(Socket.AVG.Download) || 0 +'</h3>'
+                }]
+            })
+        ],
+    });
+    const AVGUploadChart = new Chartist.Pie('#AVGUploadChart', {
+        series: [Socket.AVG.Upload || 0 ],
+        labels: ['']
+    }, {
+        donut: true,
+        donutWidth: 20,
+        startAngle: 210,
+        total: Socket.AVG.Upload <= 5242880 ? 5242880 : 131072000, //5242880
+        showLabel: false,
+        plugins: [
+            Chartist.plugins.fillDonut({
+                items: [{
+                    content: '<i class="fa fa-tachometer"></i>',
+                    position: 'bottom',
+                    offsetY : 10,
+                    offsetX: -2
+                }, {
+                    content: '<h3 id="AVGUploadChartH3">'+ prettierBytes(Socket.AVG.Upload) || 0 +'</h3>'
+                }]
+            })
+        ],
+    });
+    $scope.AVGPing = Socket.AVG.Ping.toFixed(2);
     $scope.LastPing = Socket.Ping[Socket.Ping.length > 0 ? Socket.Ping.length - 1 : 0].value || 0;
     let d = new Date(Socket.labels[Socket.labels.length > 0 ? Socket.labels.length - 1 : 0] || 0);
     $scope.LastTest = moment(d).format("DD MMM YYYY kk:mm");
@@ -228,58 +286,45 @@ app.controller('home', function ($scope, Socket, $rootScope, $state) {
             labels: Socket.labels,
             series: [Socket.Ping]
         });
-        LastDownloadChart.update({
+
+        $scope.updateChart(LastDownloadChart, {
             series: [Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].value || 0 ],
-            labels: ['', '']
-        }, {
-            donut: true,
-            donutWidth: 20,
-            startAngle: 210,
-            total: Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].value < 5 ? 5 : 125,
-            showLabel: false,
-            plugins: [
-                Chartist.plugins.fillDonut({
-                    items: [{
-                        content: '<i class="fa fa-tachometer"></i>',
-                        position: 'bottom',
-                        offsetY : 10,
-                        offsetX: -2
-                    }, {
-                        content: '<h3 id="LastDownloadChartH3">'+ Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].meta || 0 +'</h3>'
-                    }]
-                })
-            ],
-        });
-        document.getElementById('LastDownloadChartH3').innerHTML = Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].meta || 0;
-        LastUploadChart.update({
+            labels: ['']
+        }, {total: Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].value < 5 ? 5 : 125}, "LastDownloadChartH3", Socket.Download[Socket.Download.length > 0 ? Socket.Download.length - 1 : 0].meta || 0);
+
+        $scope.updateChart(LastUploadChart, {
             series: [Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].value || 0 ],
-            labels: ['', '']
-        }, {
-            donut: true,
-            donutWidth: 20,
-            startAngle: 210,
-            total: Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].value < 5 ? 5 : 125,
-            showLabel: false,
-            plugins: [
-                Chartist.plugins.fillDonut({
-                    items: [{
-                        content: '<i class="fa fa-tachometer"></i>',
-                        position: 'bottom',
-                        offsetY : 10,
-                        offsetX: -2
-                    }, {
-                        content: '<h3 id="LastUploadChartH3">'+ Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].meta || 0 +'</h3>'
-                    }]
-                })
-            ],
-        });
-        document.getElementById('LastUploadChartH3').innerHTML = Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].meta || 0;
+            labels: ['']
+        }, {total: Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].value < 5 ? 5 : 125}, "LastUploadChartH3", Socket.Upload[Socket.Upload.length > 0 ? Socket.Upload.length - 1 : 0].meta || 0);
+
+        $scope.updateChart(AVGDownloadChart, {
+            series: [Socket.AVG.Download || 0 ],
+            labels: ['']
+        }, {total: Socket.AVG.Download <= 5242880 ? 5242880 : 131072000}, "AVGDownloadChartH3", prettierBytes(Socket.AVG.Download) || 0);
+
+        $scope.updateChart(AVGUploadChart, {
+            series: [Socket.AVG.Upload || 0 ],
+            labels: ['']
+        }, {total: Socket.AVG.Upload <= 5242880 ? 5242880 : 131072000}, "AVGUploadChartH3", prettierBytes(Socket.AVG.Upload) || 0);
+
         $scope.LastPing = Socket.Ping[Socket.Ping.length > 0 ? Socket.Ping.length - 1 : 0].value || 0;
         let d = new Date(Socket.labels[Socket.labels.length > 0 ? Socket.labels.length - 1 : 0] || 0);
         $scope.LastTest = moment(d).format("DD MMM YYYY kk:mm");
+        $scope.AVGPing = Socket.AVG.Ping.toFixed(2);
         $scope.safeApply();
         console.log("Chart updated.");
     });
+
+    $scope.updateChart = function (Chart, Data, options, H3ID, H3) {
+        Chart.update(Data, options, true);
+        if (H3 && H3ID){
+            document.getElementById(H3ID).innerHTML = H3;
+        }
+    };
+
+    $scope.openMenu = function ($mdMenu, ev) {
+        $mdMenu.open(ev);
+    };
 });
 
 app.config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider) {
